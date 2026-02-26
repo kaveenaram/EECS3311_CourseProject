f95fdca9f93e3c3ca7218f7eff5f846d5b6a6cc3
kaveenaram <kaveenaram@outlook.com>
2026-02-26 11:19:13 -0500

    added bookingservice and user class

---
+++ backend/BookingService.java
@@ -0,0 +1,13 @@
+package EECS3311_CourseProject.backend;
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+    
+BookingService contains all main functions for the app.
+
+/write further description on how it functions
+    
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+public class BookingService {
+
+}
---
+++ backend/User.java
@@ -0,0 +1,17 @@
+package EECS3311_CourseProject.backend;
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+    
+Class User defines the client who requests and cancels appointments.
+    
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+public class User {
+    // private attributes
+
+    public User(){
+        // constructor
+    }
+
+    // get name function would be helpful, used when booking / cancelling
+}
