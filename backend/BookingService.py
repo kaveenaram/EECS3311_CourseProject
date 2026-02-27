b87cc7a4a36894ff63399f6e56789a73214ab884
kaveenaram <kaveenaram@outlook.com>
2026-02-26 22:04:12 -0500

    decided on using  python rather than java

---
+++ backend/BookingService.py
@@ -0,0 +1,3 @@
+# BookingService contains all main functions for the app.
+# write further description on how it functions
+    
---
+++ backend/User.py
@@ -0,0 +1,2 @@
+# Abstract class User defines User behaviour and characteristics
+# remember to add getter and setter functions for the name and other attributes if needed
