9a6795f6f5915e687843431edd7f206fc6d23fac
duljadj <81400677+duljadj@users.noreply.github.com>
2026-03-03 00:08:07 -0500

    Update payment_method.py

--- backend/patterns/strategy/payment_method.py
+++ backend/patterns/strategy/payment_method.py
@@ -1,1 +1,15 @@
+from abc import ABC, abstractmethod
+from entities.payment_result import PaymentResult
 
+#abstract class 
+class paymentMethod(ABC):
+  @abstractmethod
+  def validate(self) -> bool:
+    pass 
+
+  @abstractmethod
+  def process(self, amount:float)->PaymentResult:
+    pass
+
+    
+  
