0a01037cecc19a7afc1ed24558e5dd891980a1d4
Dulja Ranathunga <duljaranathunga1@gmail.com>
2026-03-03 17:59:32 -0500

    modity

--- backend/entities/payment_result.py
+++ backend/entities/payment_result.py
@@ -1,12 +1,17 @@
 from datetime import datetime
 
 class PaymentResult:
-  def __init__(self,transactionID:str, success:bool, base_price:float, timestamp:datetime = None):
+  def __init__(self,transactionID:str, success:bool, base_price:float ):
     self.transactionID = transactionID
     self.success = success
-    self.timestamp = timestamp
+    self.timestamp = datetime.now()
     self.base_price = base_price
 
+  def __str__(self):
+    return(
+      f"Transaction ID: {self.transactionID} \n"
+      f"Success: {self.success} \n"
+      f"Timestamp: {self.timestamp} \n"
+      f"Base Price: {self.base_price}")
+
     
-
-
