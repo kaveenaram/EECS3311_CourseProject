7f18db1f4e30fe2e048ed4734697cca87d2a5c02
duljadj <81400677+duljadj@users.noreply.github.com>
2026-03-03 00:24:10 -0500

    Update payment_result.py

--- backend/entities/payment_result.py
+++ backend/entities/payment_result.py
@@ -1,1 +1,6 @@
+from datetime import datetime
 
+class PaymentResult:
+  def __init__(self,transactionID:str, success:bool, timestamp:datetime = None,base_price:float):
+    self.transactionID = transaction
+    
