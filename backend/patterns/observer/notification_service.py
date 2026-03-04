aabf51ede96f74c8a9d638a3f8e36c09b29b467e
tashiyas <126813518+tashiyas@users.noreply.github.com>
2026-03-04 18:21:06 -0500

    Update notification_service.py

--- backend/patterns/observer/notification_service.py
+++ backend/patterns/observer/notification_service.py
@@ -12,6 +12,6 @@ class NotificationService(Subject):
   def detach(self, observer: Observer):
     self.observers.remove(observer) #removes observer from the list
 
-  def notify_observers(self, message: str):
+  def notifyObservers(self, message: str):
       for obs in self.observers:
         obs.update(message)
