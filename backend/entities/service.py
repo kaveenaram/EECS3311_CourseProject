11856779e8ca0eefa1efa545151aa8ab2ca450d6
TerryDont <121741792+TerryDont@users.noreply.github.com>
2026-03-04 00:38:18 -0500

    Update service.py
    UC1
    Stores service details
    Links service to consultant
    Provides readable output

--- backend/entities/service.py
+++ backend/entities/service.py
@@ -1,1 +1,11 @@
+class Service:
 
+  def __init__(self, service_id: str, title: str, duration: int, price: float, consultant):
+      self.service_id = service_id
+      self.title = title
+      self.duration = duration
+      self.price = price
+      self.consultant = consultant
+
+  def __str__(self):
+      return f"{self.title} | {self.duration} mins | ${self.price}"
