e92b2a12b6ed69831467c943685da7931083f937
tashiyas <126813518+tashiyas@users.noreply.github.com>
2026-03-04 12:24:48 -0500

    Update subject.py

--- backend/patterns/observer/subject.py
+++ backend/patterns/observer/subject.py
@@ -1,3 +1,9 @@
 class Subject:
   def attach(self, observer: Observer):
     raise NotImplementedError 
+
+  def detach(self, observer: Observer):
+    raise NotImplementedError
+
+  def notifyObservers(self, msg : Str):
+    raise NotImplementedError
