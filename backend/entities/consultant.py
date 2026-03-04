4444ab268dbac7413a70c80f88fa3cb5aa5840bf
kaveenaram <kaveenaram@outlook.com>
2026-03-04 01:19:57 -0500

    added login, logout, self.approved in consultant.py

--- backend/entities/consultant.py
+++ backend/entities/consultant.py
@@ -9,6 +9,16 @@ class Consultant(User):
       super().__init__(user_id, name, email)
       self.services: List[Service] = []
       self.timeslots: List[TimeSlot] = []
+      self.approved: bool = False
+
+  def logIn(self, password: str) -> bool:
+        # if a consultant is not approved by an admin yet, they cannot log in
+        if not self.approved:
+            return False
+        return super().logIn(password)
+        
+  def logOut(self) -> None:
+                return super().logOut()
 
   def add_service(self, service: Service) -> None:
       self.services.append(service)
@@ -21,3 +31,5 @@ class Consultant(User):
 
   def get_available_timeslots(self) -> List[TimeSlot]:
       return [slot for slot in self.timeslots if slot.available]
+  
+  
