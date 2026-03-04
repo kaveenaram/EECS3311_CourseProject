d850dab19cf8faabfb5d696885d99e595578da38
Dulja Ranathunga <duljaranathunga1@gmail.com>
2026-03-03 21:42:06 -0500

    modified

--- backend/services/payment_service.py
+++ backend/services/payment_service.py
@@ -1,1 +1,23 @@
+from typing import List
+from entities.payment_result import PaymentResult
+
+#this class process payments, update booking state,stores history and provide access to the history 
+class PaymentService:
+
+    def __init__(self):
+        #initialising a list to store all the payments processed by the system 
+        self.payment_history = List[PaymentResult] = []
+
+    def process_payment(self,booking,payment_method,amount:float) ->PaymentResult:
+        #process payment using the selected payment method 
+        result = payment_method.process(amount)   
+
+        if result.success:
+            booking.mark_as_paid()
+            self.payment_history.append(result)
+
+        return result
+    
+    def get_payment_history(self) -> List[PaymentResult]:
+        return self.payment_history
 
