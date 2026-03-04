069c19790368d54dca486cd5e14deefb80a53066
Dulja Ranathunga <duljaranathunga1@gmail.com>
2026-03-03 20:52:04 -0500

    modified

--- backend/entities/user.py
+++ backend/entities/user.py
@@ -1,1 +1,17 @@
+from abc import ABC, abstractmethod
+
+class User(ABC):
+
+    def __init__(self, user_id:str, name:str, email:str):
+        self.user_id = user_id
+        self.name = name 
+        self.email = email
+
+    @abstractmethod
+    def logIn(self,password:str) ->bool:
+        pass
+
+    @abstractmethod
+    def logOut(self) ->None:
+        pass 
 
