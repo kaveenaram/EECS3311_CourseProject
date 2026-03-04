7185b75afba8da56c775596f5ed995b6221cd003
Dulja Ranathunga <duljaranathunga1@gmail.com>
2026-03-03 19:07:01 -0500

    modifications

--- backend/entities/payment_result.py
+++ backend/entities/payment_result.py
@@ -1,17 +1,17 @@
 from datetime import datetime
 
 class PaymentResult:
-  def __init__(self,transactionID:str, success:bool, base_price:float ):
+  def __init__(self,transactionID:str, success:bool, amount:float ):
     self.transactionID = transactionID
     self.success = success
     self.timestamp = datetime.now()
-    self.base_price = base_price
+    self.amount = amount
 
   def __str__(self):
     return(
       f"Transaction ID: {self.transactionID} \n"
       f"Success: {self.success} \n"
       f"Timestamp: {self.timestamp} \n"
-      f"Base Price: {self.base_price}")
+      f"Base Price: {self.amount}")
 
     
--- backend/patterns/strategy/credit_card.py
+++ backend/patterns/strategy/credit_card.py
@@ -1,1 +1,62 @@
+import time
+import uuid
+from datetime import datetime
+from .payment_method import PaymentMethod
+from entities.payment_result import PaymentResult
+
+class CreditCard(PaymentMethod):
+    
+    def __init__(self,card_number: str, expiry : str, cvv:str ):
+        self.card_number = card_number
+        self.expiry = expiry
+        self.cvv = cvv
+
+    def validate(self) -> bool:
+        #16 digit check
+        if len(self.card_number) != 16 or not self.card_number.isdigit():
+            return False
+        
+        #cvv
+        if len(self.cvv) not in (3,4) or not self.cvv.isdigit():
+            return False 
+        
+        #expiry date - assuming its on MMYY format 
+        now = datetime.now()
+        current_year = now.year
+        current_month = now.month
+
+        try:
+            card_year = int(self.expiry[2:]) + 2000
+            card_month = int(self.expiry[0:2])
+            if not 1 <= card_month <=12:
+                return False
+        except ValueError:
+            return False #invalid format
+        
+
+
+
+        if card_year < current_year:
+            return False 
+        elif card_year == current_year:
+            if card_month < current_month:
+                return False
+        
+        return True 
+    
+    def process(self,amount:float) ->PaymentResult:
+        if not self.validate():
+            raise Exception("Invalid Credit Card details")
+        
+        time.sleep(3) #simulating processing delay 
+
+        transactionID = str(uuid.uuid4())
+
+        return PaymentResult(transactionID,True,amount)
+        
+
+
+
+
+
 
