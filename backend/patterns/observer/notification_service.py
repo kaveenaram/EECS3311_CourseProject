c8d4de6e0ae2ce7a20ba2984f2bc650bf7c3738c
tashiyas <126813518+tashiyas@users.noreply.github.com>
2026-03-04 12:30:27 -0500

    Update notification_service.py

--- backend/patterns/observer/notification_service.py
+++ backend/patterns/observer/notification_service.py
@@ -1,7 +1,14 @@
 class NotificationService(Subject):
 
   def __init__(self):
-    self.observers = []
+    self.observers = [] #creates an empty list that stores all Observers
 
   def attach(self, observer: Observer):
-    
+    self.observers.append(observer) #adds the observer to the list created above
+
+  def detach(self, observer: Observer):
+    self.observers.remove(observer) #removes observer from the list
+
+  def notifyObservers(self, message: str):
+      for obs in self.observers:
+        obs.update(message)
