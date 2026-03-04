a4b57f0109afbb190bec4d1284bf2c194bcb4513
tashiyas <126813518+tashiyas@users.noreply.github.com>
2026-03-03 22:08:29 -0500

    Update system_policy.py

--- backend/entities/system_policy.py
+++ backend/entities/system_policy.py
@@ -1,1 +1,12 @@
-
+class SystemPolicy:
+    def __init__(self, cancellationRules = "", pricingStrategy = "", refundPolicy=""):
+        self.cancellationRules = cancellationRules
+        self.pricingStrategy = pricingStrategy
+        self.refundPolicy = refundPolicy
+    def updateCancellationRules(self, rules: str): #U12: define policies. Replaces existing cancellation rules with new ones.
+      self.cancellationRules = rules
+    def updatePricingStrategy(self, strategy: str): #updates pricing strategy
+      self.pricingStrategy = strategy
+    def updateRefundPolicy(self, refund: str): #how and when refunds are issued
+      self.refundPolicy = refund
+      
