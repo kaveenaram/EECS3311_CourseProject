5c08a2a82373b95cec782958eb7034b01a6e140f
tashiyas <126813518+tashiyas@users.noreply.github.com>
2026-03-04 12:35:51 -0500

    Update client_consultant.py

--- backend/patterns/observer/client_consultant.py
+++ backend/patterns/observer/client_consultant.py
@@ -1,1 +1,8 @@
+class Client(Observer): #inherits from the observer interface
+    def update(self, message: str): 
+        print(f"[Client Notification] {message}") #implements how the notification is received by the client
 
+
+class Consultant(Observer):
+    def update(self, message: str):
+        print(f"[Consultant Notification] {message}") #implements how the notification is received by the consultant
