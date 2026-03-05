48c4a65c334fdf4b5569fbe707e91bca973f8bcd
Dulja Ranathunga <duljaranathunga1@gmail.com>
2026-03-04 23:50:31 -0500

    some error fixes

--- backend/services/payment_service.py
+++ backend/services/payment_service.py
@@ -1,5 +1,7 @@
 from typing import List
 from entities.payment_result import PaymentResult
+from entities.booking import Booking
+from patterns.strategy.payment_method import PaymentMethod
 
 #this class process payments, update booking state,stores history and provide access to the history 
 class PaymentService:
@@ -8,15 +10,17 @@ class PaymentService:
         #initialising a list to store all the payments processed by the system 
         self.payment_history: List[PaymentResult] = []
 
-    def process_payment(self,booking,payment_method,amount:float) ->PaymentResult:
+    def process_payment(self,booking :Booking ,payment_method: PaymentMethod,amount:float) ->PaymentResult | None:
         #process payment using the selected payment method 
         result = payment_method.process(amount)   
 
         if result.success:
             booking.paid(amount)
             self.payment_history.append(result)
-
-        return result
+            return result
+        else:
+            print("Payment Failed. Please try again")
+            return None
     
     def get_payment_history(self) -> List[PaymentResult]:
         return self.payment_history
