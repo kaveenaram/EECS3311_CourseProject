4f4f71f0f794e2aea5157ac0e1734621b64049a1
Dulja Ranathunga <duljaranathunga1@gmail.com>
2026-03-05 21:09:37 -0500

    some fixes

--- backend/main.py
+++ backend/main.py
@@ -5,6 +5,11 @@ from entities.client import Client
 from entities.service import Service
 from entities.timeslot import TimeSlot
 
+from patterns.strategy.credit_card import CreditCardPayment
+from patterns.strategy.debit_card import DebitCardPayment
+from patterns.strategy.paypal import PayPalPayment
+from patterns.strategy.bank_transfer import BankTransfer
+
 def main():
     available_services = AvailabilityService()
     booking_service = BookingService()
@@ -24,7 +29,8 @@ def main():
 
     if choice == "1":
        services = available_services.browse_services()
-       print(services)
+       for s in services:
+           print(s)
        
     elif choice == "2":
         userid = input("Enter userId: ")
@@ -34,20 +40,31 @@ def main():
         slot = input("Enter TimeSlot: ") #in phase 2 this will be changed to actually create the timeslot object, for now it assume this does that 
 
         services = available_services.browse_services() 
-        if service_choice in services:
-            consultant = service_choice.consultant #consultant that provide that service 
+
+        #finding the service object 
+        selected_service = None 
+        for s in services:
+            if s.name == service_choice:
+                selected_service =s
+                break
+
+        if selected_service:
+            consultant = selected_service.consultant
         else:
             raise Exception("Invalid Service")
-        
 
         client = Client(userid,name,email)
+    
+        #time slot object 
+        timeslot = TimeSlot(slot)
+
         book = booking_service.create_booking(client,consultant,service_choice,slot)
 
 
         #consultant accepting or rejecting 
         available_slot = consultant.get_available_timeslots()
 
-        if slot in available_slot:
+        if timeslot in available_slot:
             booking_service.confirm_booking(book)
         else:
             booking_service.reject_booking(book)
@@ -56,15 +73,34 @@ def main():
         print("If you haven't made a booking yet, please go back and make a booking\n")
 
         booking_id = input("Enter Booking Id: ")
-        payment_method = ("Enter payment Method: ") #in phase 2 this will ask for the correct choice like credit or debit etc and will direct it to enter the correct details needed to actually create the PaymentMethod Object 
+
+
+        print("1. Credit Card")
+        print("2. Debit Card")
+        print("3. PayPal")
+        print("4. Bank transfer")
+
+        payment_choice = input("Enter payment Method: ") #in phase 2 this will ask for the correct choice like credit or debit etc and will direct it to enter the correct details needed to actually create the PaymentMethod Object 
         amount = float(input("Enter amount"))
 
         booking = booking_service.get_booking(booking_id)
         
-        if booking:
-            result = payment_service.process_payment(booking,payment_method,amount)
-        else:
+        if not booking:
             raise Exception("booking id entered is invalid")
+        
+        #create correct payment strategy 
+        if payment_choice == "1":
+            payment_method = CreditCardPayment()
+        elif payment_choice == "2":
+            payment_method = DebitCardPayment()
+        elif payment_choice == "3":
+            payment_method = PayPalPayment()
+        elif payment_choice == "4":
+            payment_method = BankTransfer()
+        else:
+            raise Exception("Invalid payment method")
+        
+        result = payment_service.process_payment(booking, payment_method, amount)
         
         if result is not None:
             print("Payment Processed Successfully")
