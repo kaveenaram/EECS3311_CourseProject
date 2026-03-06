5171d52a3f27fe9d3fb573dc1fd774fa9a5e2602
Dulja Ranathunga <duljaranathunga1@gmail.com>
2026-03-05 23:36:03 -0500

    circular import fix

--- backend/entities/service.py
+++ backend/entities/service.py
@@ -1,3 +1,8 @@
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from entities.consultant import Consultant
+
 
 class Service:
     def __init__(self, service_id: str, serviceName: str, duration: int, price: float, consultant: "Consultant"):
