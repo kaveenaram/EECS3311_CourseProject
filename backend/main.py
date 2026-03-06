04e12fcaff4ea608975ad3304bd6db9b9cdf78b9
Dulja Ranathunga <duljaranathunga1@gmail.com>
2026-03-05 23:12:19 -0500

    adding a loop

--- backend/main.py
+++ backend/main.py
@@ -25,93 +25,96 @@ def main():
     print("3. Make a Payment")
     print("4. Exit")
 
-    choice = input("Select Option: ")
 
-    if choice == "1":
-       services = available_services.browse_services()
-       for s in services:
-           print(s)
-       
-    elif choice == "2":
-        userid = input("Enter userId: ")
-        name = input("Enter name: ")
-        email = input("Enter email: ")
-        service_choice = input("Enter Service: ") #in phase 2 this will be changed to actually create the service object, for now it assume this does that 
-        slot = input("Enter TimeSlot: ") #in phase 2 this will be changed to actually create the timeslot object, for now it assume this does that 
+    while True:
+        choice = input("Select Option: ")
 
-        services = available_services.browse_services() 
+        if choice == "1":
+           services = available_services.browse_services()
+           for s in services:
+               print(s)
 
-        #finding the service object 
-        selected_service = None 
-        for s in services:
-            if s.name == service_choice:
-                selected_service =s
-                break
+        elif choice == "2":
+            userid = input("Enter userId: ")
+            name = input("Enter name: ")
+            email = input("Enter email: ")
+            service_choice = input("Enter Service: ") #in phase 2 this will be changed to actually create the service object, for now it assume this does that 
+            slot = input("Enter TimeSlot: ") #in phase 2 this will be changed to actually create the timeslot object, for now it assume this does that 
 
-        if selected_service:
-            consultant = selected_service.consultant
+            services = available_services.browse_services() 
+
+            #finding the service object 
+            selected_service = None 
+            for s in services:
+                if s.name == service_choice:
+                    selected_service =s
+                    break
+
+            if selected_service:
+                consultant = selected_service.consultant
+            else:
+                raise Exception("Invalid Service")
+
+            client = Client(userid,name,email)
+
+            #time slot object 
+            timeslot = TimeSlot(slot)
+
+            book = booking_service.create_booking(client,consultant,service_choice,slot)
+
+
+            #consultant accepting or rejecting 
+            available_slot = consultant.get_available_timeslots()
+
+            if timeslot in available_slot:
+                booking_service.confirm_booking(book)
+            else:
+                booking_service.reject_booking(book)
+
+        elif choice == "3":
+            print("If you haven't made a booking yet, please go back and make a booking\n")
+
+            booking_id = input("Enter Booking Id: ")
+
+
+            print("1. Credit Card")
+            print("2. Debit Card")
+            print("3. PayPal")
+            print("4. Bank transfer")
+
+            payment_choice = input("Enter payment Method: ") #in phase 2 this will ask for the correct choice like credit or debit etc and will direct it to enter the correct details needed to actually create the PaymentMethod Object 
+            amount = float(input("Enter amount"))
+
+            booking = booking_service.get_booking(booking_id)
+
+            if not booking:
+                raise Exception("booking id entered is invalid")
+
+            #create correct payment strategy 
+            if payment_choice == "1":
+                payment_method = CreditCard()
+            elif payment_choice == "2":
+                payment_method = DebitCard()
+            elif payment_choice == "3":
+                payment_method = Paypal()
+            elif payment_choice == "4":
+                payment_method = BankTransfer()
+            else:
+                raise Exception("Invalid payment method")
+
+            result = payment_service.process_payment(booking, payment_method, amount)
+
+            if result is not None:
+                print("Payment Processed Successfully")
+            else:
+                print("Something went Wrong, please try again")
+
+        elif choice == "4":
+            print("Goodbye")
+            break
+
         else:
-            raise Exception("Invalid Service")
-
-        client = Client(userid,name,email)
-    
-        #time slot object 
-        timeslot = TimeSlot(slot)
-
-        book = booking_service.create_booking(client,consultant,service_choice,slot)
-
-
-        #consultant accepting or rejecting 
-        available_slot = consultant.get_available_timeslots()
-
-        if timeslot in available_slot:
-            booking_service.confirm_booking(book)
-        else:
-            booking_service.reject_booking(book)
-
-    elif choice == "3":
-        print("If you haven't made a booking yet, please go back and make a booking\n")
-
-        booking_id = input("Enter Booking Id: ")
-
-
-        print("1. Credit Card")
-        print("2. Debit Card")
-        print("3. PayPal")
-        print("4. Bank transfer")
-
-        payment_choice = input("Enter payment Method: ") #in phase 2 this will ask for the correct choice like credit or debit etc and will direct it to enter the correct details needed to actually create the PaymentMethod Object 
-        amount = float(input("Enter amount"))
-
-        booking = booking_service.get_booking(booking_id)
-        
-        if not booking:
-            raise Exception("booking id entered is invalid")
-        
-        #create correct payment strategy 
-        if payment_choice == "1":
-            payment_method = CreditCard()
-        elif payment_choice == "2":
-            payment_method = DebitCard()
-        elif payment_choice == "3":
-            payment_method = Paypal()
-        elif payment_choice == "4":
-            payment_method = BankTransfer()
-        else:
-            raise Exception("Invalid payment method")
-        
-        result = payment_service.process_payment(booking, payment_method, amount)
-        
-        if result is not None:
-            print("Payment Processed Successfully")
-        else:
-            print("Something went Wrong, please try again")
-
-    elif choice == "4":
-        print("Goodbye")
-
-    else:
-        print("Invalid Choice")
+            print("Invalid Choice")
 
 if __name__ == "__main__":
     main()
