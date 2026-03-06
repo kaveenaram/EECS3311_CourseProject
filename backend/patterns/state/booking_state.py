379d260a1d7d8fee9da6cf68897a21d35d8b57d7
Dulja Ranathunga <duljaranathunga1@gmail.com>
2026-03-06 00:11:38 -0500

    circular import fixes

--- backend/patterns/state/booking_state.py
+++ backend/patterns/state/booking_state.py
@@ -10,16 +10,16 @@ class BookingState(ABC):
     # initial state when a client submits a booking request is the requested state
     # interface for booking states
     # concrete states will override allowed transitions
-    def confirm(self, booking: Booking):
+    def confirm(self, booking: "Booking"):
         raise Exception("Invalid state transition: Booking cannot be confirmed from the current state")
-    def reject(self, booking: Booking):
+    def reject(self, booking: "Booking"):
         raise Exception("Invalid state transition: Booking cannot be rejected from the current state")
-    def pending_payment(self, booking: Booking):
+    def pending_payment(self, booking: "Booking"):
         raise Exception("Invalid state transition: Booking cannot be marked as pending payment from the current state")
-    def paid(self, booking: Booking, amount: float):
+    def paid(self, booking: "Booking", amount: float):
         raise Exception("Invalid state transition: Booking cannot be marked as paid from the current state")
-    def cancel(self, booking: Booking):
+    def cancel(self, booking: "Booking"):
         raise Exception("Invalid state transition: Booking cannot be cancelled from the current state")
-    def complete(self, booking: Booking):
+    def complete(self, booking: "Booking"):
         raise Exception("Invalid state transition: Booking cannot be marked as completed from the current state")
 
