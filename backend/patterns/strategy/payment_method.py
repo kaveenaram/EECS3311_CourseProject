328345c00fb1ab92f351b8613268b9cead8e9c48
Dulja Ranathunga <duljaranathunga1@gmail.com>
2026-03-05 22:59:37 -0500

    fix

--- backend/patterns/strategy/payment_method.py
+++ backend/patterns/strategy/payment_method.py
@@ -2,7 +2,7 @@ from abc import ABC, abstractmethod
 from entities.payment_result import PaymentResult
 
 #abstract class 
-class paymentMethod(ABC):
+class PaymentMethod(ABC):
   @abstractmethod
   def validate(self) -> bool:
     pass 
