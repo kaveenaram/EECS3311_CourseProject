0e3deee26f71497332703a39a72d43bee6475f0a
Dulja Ranathunga <duljaranathunga1@gmail.com>
2026-03-31 22:38:20 -0400

    disabling some stuff for now

--- frontend/src/pages/ConsultantDashboard.js
+++ frontend/src/pages/ConsultantDashboard.js
@@ -1,14 +1,14 @@
-import AvailabilityManager from "../components/AvailabilityManager";
-import BookingRequests from "../components/BookingRequests";
-import ScheduleView from "../components/ScheduleView";
+//import AvailabilityManager from "../components/AvailabilityManager";
+//import BookingRequests from "../components/BookingRequests";
+//import ScheduleView from "../components/ScheduleView";
 
 function ConsultantDashboard(){
     return (
         <div>
             <h1>Consultant Dashboard</h1>
-            <AvailabilityManager/>
+            {/*<AvailabilityManager/>
             <BookingRequests/>
-            <ScheduleView/>
+            <ScheduleView/>*/}
         </div>
     );
 }
